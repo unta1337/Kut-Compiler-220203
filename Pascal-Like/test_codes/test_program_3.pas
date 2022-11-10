3c9ee5ae75bf2ab7379e7f550eba104fd160d814
unta1337 <firstapple42@gmail.com>
2022-11-10 21:12:53 +0900

    예제 코드 들여쓰기 수정 및 주석 추가

--- Pascal-Like/test_codes/test_program_3.pas
+++ Pascal-Like/test_codes/test_program_3.pas
@@ -1,7 +1,16 @@
+/*
+ * 파스칼 비슷한 언어 렉서, 파서 구현.
+ *
+ * 아래의 코드는 정상적인 코드.
+ * 이유: 중첩된 begin-end 구문 사용 가능.
+ *      두 개의 구문이 존재하면 이를 세미콜론으로 구분.
+ *      아래의 경우 if-then 구문과 while-do 구문이 존재, 내부의 begin-end 구문은 while-do 안의 구문이므로 while-do에 포함됨.
+ */
+
 begin
-if a < b then c := 1;
-while d > 0 do
-begin
-e := 2 + 3 - 4
-end
+    if a < b then c := 1;
+    while d > 0 do
+    begin
+        e := 2 + 3 - 4
+    end
 end
